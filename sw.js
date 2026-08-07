69f201e22e0dd0421a5e7ba4d11dfc92233f5bdb
Danbridge777 <sv6czdxdqg@privaterelay.appleid.com>
2026-08-07 19:02:46 +0800

    Fade long weekly lesson cards

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='danbridge-v11-premium-week-calendar-14';
+const CACHE_NAME='danbridge-v11-premium-week-calendar-15';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
