01bafd8a34ae806d041a3bde1b1fba6603c0738c
Danbridge777 <sv6czdxdqg@privaterelay.appleid.com>
2026-08-07 18:43:50 +0800

    Refine premium weekly calendar design

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='danbridge-v11-calendar-cleanup-12';
+const CACHE_NAME='danbridge-v11-premium-week-calendar-13';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
