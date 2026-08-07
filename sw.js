b9459df804a9b9a1e554f030d73b3b69b0754490
Danbridge777 <sv6czdxdqg@privaterelay.appleid.com>
2026-08-07 11:16:35 +0800

    Fix clipped weekly calendar columns

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='danbridge-v11-all-hours-draggable-6';
+const CACHE_NAME='danbridge-v11-week-horizontal-overflow-7';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
