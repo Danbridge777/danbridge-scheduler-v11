3887814c2df01754959937a97197bd78097b5d78
Danbridge777 <sv6czdxdqg@privaterelay.appleid.com>
2026-08-08 13:52:47 +0800

    Compact empty gap analysis days

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='danbridge-v11-weekly-gap-layout-18';
+const CACHE_NAME='danbridge-v11-compact-empty-gap-19';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
