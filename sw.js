c7e68316d4abb2ee359a6fd6c3d9ae1bad33e677
Danbridge777 <sv6czdxdqg@privaterelay.appleid.com>
2026-08-08 13:48:50 +0800

    Fix weekly gap analysis layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='danbridge-v11-weekly-gap-analysis-17';
+const CACHE_NAME='danbridge-v11-weekly-gap-layout-18';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
